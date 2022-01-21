5c541775ad5f1136e2f3c70bdf6a3a0ae97e68fd
StasEngel <69434834+StasEngel@users.noreply.github.com>
2022-01-21 22:06:02 +0200

    ggg

--- Stocks_App/urls.py
+++ Stocks_App/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('add_transaction', views.add_transaction, name='add_transaction'),
+    path('query_results', views.query_results, name='query_results'),
+    path('buy_stocks', views.buy_stocks, name='buy_stocks'),
 ]
--- Stocks_App/views.py
+++ Stocks_App/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+
+def add_transaction(request):
+    return render(request, 'add_transaction.html')
+
+
+def query_results(request):
+    return render(request, 'query_results.html')
+
+
+def buy_stocks(request):
+    return render(request, 'buy_stocks.html')
